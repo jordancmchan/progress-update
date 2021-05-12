6c84db9834e373d7f1131b88a19ef58fca3686f9
jordancmchan <jordancmchan@hotmail.com>
2021-05-12 17:51:26 -0400

    first push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+      <h1>How I'm Improving</h1>
+      <h2>Currently working on: Javascript 30 </h2>
+      <p>30 day vanilla coding challenge</p>
+      <p>build 30 projects in 30 days</p>
+      <a href="https://javascript30.com">SOURCE</a>
       </header>
+      <body>
+        <div className="contentContainer">
+          <div className="screenShotContainer">
+            <a href ="https://stoic-feynman-96a430.netlify.app"> 
+            <img src="https://placedog.net/250" alt=""/>
+            </a>
+          </div>
+          <div className="contentDesContainer">
+            <h2>Project Name</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum quod sint deleniti, quae soluta molestias magni. Consectetur aut accusantium quo.</p>
+          </div>
+          <div className="infoContainer">
+            <p>Uploaded On: 152131</p>
+            <nav>
+              <li>github link</li>
+              <li>live site link</li>
+            </nav>
+          </div>
+        </div>
+      </body>
     </div>
   );
 }
