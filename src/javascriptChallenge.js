c0786eac8e8d72b6666af606d4ce074b7f11ec90
jordancmchan <jordancmchan@hotmail.com>
2021-05-17 17:31:01 -0400

    added newest daily challenge

--- src/javascriptChallenge.js
+++ src/javascriptChallenge.js
@@ -1,11 +1,13 @@
 import javascript01 from './assets/javascript01.png'
 import javascript02 from './assets/javascript02.png'
 import javascript03 from './assets/javascript03.png'
+import javascript04 from './assets/javascript04.png'
 
 const javascriptChallenges = [
     {image: javascript01 , title: 'Javascript DrumKit', description: 'Created a javascript drumkit by connecting audio drum sounds to event listeners on the keyboard', liveLink: 'https://stoic-feynman-96a430.netlify.app', key: '1', date: "May 16 2021" },
     {image: javascript02 , title: 'Javascript CSS Clock', description: 'Using CSS and Javascript, I created a functional accurate clock', liveLink: 'https://optimistic-heisenberg-9171f4.netlify.app', key: '2', date: "May 16 2021"},
     {image: javascript03 , title: 'Playing with CSS Variables and JS', description: 'This project shows how to utilize javascript to manipulate blur, spacing and color from user range input', liveLink: 'https://wizardly-wing-1d88e5.netlify.app', key: '3', date: "May 16 2021" },
+    {image: javascript04 , title: 'Javascript Array Cardio Day 1', description: 'This excercise has me practice different array functions such as filter, map, reduce and sort', liveLink: 'https://stoic-noyce-724815.netlify.app', key: '4', date: "May 17 2021" },
 
 ];
 
