609cc8fe8280179bef188a214377118c00f9c13a
jordancmchan <jordancmchan@hotmail.com>
2021-05-17 23:39:45 -0400

    added responsivness

--- src/javascriptChallenge.js
+++ src/javascriptChallenge.js
@@ -8,7 +8,9 @@ const javascriptChallenges = [
     {image: javascript02 , title: 'Javascript CSS Clock', description: 'Using CSS and Javascript, I created a functional accurate clock', liveLink: 'https://optimistic-heisenberg-9171f4.netlify.app', key: '2', date: "May 16 2021"},
     {image: javascript03 , title: 'Playing with CSS Variables and JS', description: 'This project shows how to utilize javascript to manipulate blur, spacing and color from user range input', liveLink: 'https://wizardly-wing-1d88e5.netlify.app', key: '3', date: "May 16 2021" },
     {image: javascript04 , title: 'Javascript Array Cardio Day 1', description: 'This excercise has me practice different array functions such as filter, map, reduce and sort', liveLink: 'https://stoic-noyce-724815.netlify.app', key: '4', date: "May 17 2021" },
-
+    
 ];
 
 export default javascriptChallenges;
+
+// {image:  , title: '', description: '', liveLink: '', key: '4', date: "May 17 2021" },
